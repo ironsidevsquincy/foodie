d2aea11fc4d5b14ed48b9a841bab510c45fbbcbe
Darren Hurley <ironsidevsquincy@googlemail.com>
2012-10-11 16:59:40 +0100

    using bower

--- public/javascripts/chef.js
+++ public/javascripts/chef.js
@@ -1,19 +1,12 @@
+var chefId = window.location.pathname.split('/').pop()
 
-
-// reqwest({
-//     url: '/api/food/chefs/5076baf1382fe8d444000001'
-//   , method: 'get'
-//   , success: function (response) {
-//       console.log(response);
-//     }
-// })
-
-domready(function() {
-
-	var chef = {
-		'name': 'Yotam Ottolenghi'
-	};
-
-	ko.applyBindings(chef);
-
+reqwest({
+    url: '/api/food/chefs/' + chefId,
+    data: 'json',
+  	method: 'get',
+  	success: function (chef) {
+      domready(function() {
+        ko.applyBindings(chef);
+      })
+    }
 })
